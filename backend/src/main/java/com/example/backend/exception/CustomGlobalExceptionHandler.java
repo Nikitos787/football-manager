7b8cfd27fdf2245330ae7b58bb74b1b46245cb74
Nikita <nsalogub96@gmail.com>
2023-08-10 17:12:37 +0300

    fix exceptions1

--- backend/src/main/java/com/example/backend/exception/CustomGlobalExceptionHandler.java
+++ backend/src/main/java/com/example/backend/exception/CustomGlobalExceptionHandler.java
@@ -1,13 +1,11 @@
 package com.example.backend.exception;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
